4a04b103be44a077299a88d753055d40b6914a46
Yasufumi Misono <yasufumimisono@gmail.com>
2024-02-02 16:47:31 +0900

    revised

--- kadai_013/kadai_013.py
+++ kadai_013/kadai_013.py
@@ -1,5 +1,7 @@
+price = int(input("金額を入力してください"))
+tax = float(input("税率(%)"))
+
 def calculate_after_tax (price, tax):
   total = price + (price*(tax/100)) 
-  print(f"{total}円です")
-
-calculate_after_tax(int(input("金額を入力してください")), float(input("税率(%)")))
+  return total
+print(calculate_after_tax(price, tax))
