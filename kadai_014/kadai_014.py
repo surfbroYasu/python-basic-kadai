016caa6375cbff980bf110e22187ab70fa8253d5
Yasufumi Misono <yasufumimisono@gmail.com>
2024-02-02 18:00:23 +0900

    changing arguments

--- kadai_014/kadai_014.py
+++ kadai_014/kadai_014.py
@@ -1,8 +1,9 @@
 price1 = 100
 price2 = 200
 tax = 1.1
-def total(price1, price2):
+
+def total():
     calc = (price1 + price2)*tax
     return calc
 
-print (total(price1, price2))
+print (total())
